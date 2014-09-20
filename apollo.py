daf591c29583da92fcb9171527d7a067af27191f
Roman Land <roman.land@gmail.com>
2014-09-20 12:13:23 +0300

    stuff

--- apollo.py
+++ apollo.py
@@ -1,14 +1,13 @@
 import serial
 import math
-from flask import Flask
+from flask import Flask, jsonify, request
 import threading
-from flask import jsonify
 
 
-ser = serial.Serial('/dev/ttyUSB0', 38400, timeout=10)
+ser = serial.Serial('/dev/ttyACM1', 38400, timeout=10)
 
-pBuffer = [None for x in range(100)]
-aBuffer = [None for x in range(100)]
+pBuffer = [None for x in range(1000)]
+aBuffer = [None for x in range(1000)]
 
 bufferIdx = { 'A' : -1, 'P' : -1}
 
@@ -43,14 +42,16 @@ P0 = 101352.932
 g = 9.8 # m/s^2
 angle = 45
 aim_distance = 200
+rocket_type = 'w'
 
 def calcDistance(PsPsi, Or, rocket):
     Ps = psiToKilopascal(PsPsi)
     L = rocketsL[rocket]
     m = rocketsW[rocket]
-    O = Or * 360 / (2 * math.pi)
+    O = Or * (2 * math.pi) / 360
     v0 = math.sqrt(2 * L * ((Ps - P0)*A /m - g ))
-    R = 2 * L * math.sin(O) * (((Ps - P0) * A)/(m * g) -1)
+    R = 2 * L * math.sin(2 * O) * (((Ps - P0) * A)/(m * g) -1)
+    # print 'L',L,'m',m, 'A',A, 'Or',Or,'O',O,'PSI',PsPsi,'v0',v0,'R', R
     return R
 
 
@@ -66,17 +67,49 @@ def curBufferPos(name):
 
 
 
-def read_from_serial():
+def find_best_angle(pressure, distance, rocketType):
+    if distance >= calcDistance(pressure, 45,rocketType):
+        return False
+    else:
+        def _distance(angle):
+            return (calcDistance(pressure, angle,rocketType),angle)
+        angles = map(_distance,range(45,90))
+        angles.reverse()
+        def best_angle(a,b):
+            if b[0] < distance:
+                return b[1]
+            else:
+                return a
+        return reduce(best_angle, angles,0)
+
+
+def loop_on_serial():
     while True:
         line = ser.readline()
-        args = line.replace("\n","")
-        action = args[0][0] 
-        if action == 'A':
-            aBuffer[nextBufferPos('A')] = float(args[1:])
-        elif action == 'P':
-            pBuffer[nextBufferPos('P')] = float(args[1:])
+        read_from_serial(line)
 
-thread = threading.Thread(target=read_from_serial, args=())
+def update_angle():
+    global aim_distance
+    global rocket_type
+    global angle
+    angle = find_best_angle(pBuffer[curBufferPos('P')],aim_distance,rocket_type)
+
+def read_from_serial(line):
+    global aim_distance
+    global rocket_type
+    args = line.replace("\n","")
+    action = args[0][0] 
+    if action == 'A':
+        aBuffer[nextBufferPos('A')] = float(args[1:])
+    elif action == 'P':
+        prev_pos = curBufferPos('P')
+        pBuffer[nextBufferPos('P')] = float(args[1:])
+        if pBuffer[prev_pos] != pBuffer[curBufferPos('P')]:
+            update_angle()
+
+
+
+thread = threading.Thread(target=loop_on_serial, args=())
 thread.start()
 
 app = Flask(__name__)
@@ -87,22 +120,40 @@ def index():
         return myfile.read().replace('\n', '')
 
 
+@app.route("/distance", methods = ['POST','GET'])
+def set_distance():
+    global aim_distance
+    if request.method == 'POST':
+        aim_distance = int(request.form['set_distance'])
+        update_angle()
+        return "ok"
+
+@app.route("/launch", methods = ['POST','GET'])
+def launch():
+    global aim_distance
+    if request.method == 'POST':
+        print "launching!"
+    return "ok"
+
 @app.route("/data")
 def data():
-
+    global aim_distance
+    global rocket_type
+    global angle
     return jsonify(
         pBuffer = pBuffer, 
         aBuffer = aBuffer, 
         cur_p_index = curBufferPos('P'), 
         cur_a_index = curBufferPos('A'),
         angle = angle,
-        is_enough_pressure = aim_distance <= calcDistance(pBuffer[curBufferPos('P')], 45,'w'),
-        cur_angle_distance = calcDistance(pBuffer[curBufferPos('P')], angle,'w'),
+        is_enough_pressure = (aim_distance <= calcDistance(pBuffer[curBufferPos('P')], 45,rocket_type)),
+        cur_angle_distance = calcDistance(pBuffer[curBufferPos('P')], angle,rocket_type),
         cur_wind = calcPoly(aBuffer[curBufferPos('A')])
     )
 
 if __name__ == "__main__":
     app.run()
+
 
 
 
